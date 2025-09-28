9435089d30fc134969bed608906e3a732d3a3ed6
rtwoo <rtwoo@asu.edu>
2025-09-28 15:16:52 -0700

    fix incorrect env var name for db pass

--- pann_search.py
+++ pann_search.py
@@ -28,7 +28,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = int(os.getenv("DB_PORT", 5432))
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASSWORD")
+DB_PASS = os.getenv("DB_PASS")
 
 class AudioSimilaritySearcher:
     """
